67e629db44a5470a94b58c45dd458ca75ea82f83
Papymankan <parsa84rostami@gmail.com>
2025-09-27 13:38:03 +0330

    feat(ui) : userComponent and search input in userslist

--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -2,6 +2,8 @@ import { User } from "@/types";
 import React from "react";
 import { Button } from "./ui/Button";
 import { PencilIcon, TrashIcon } from "lucide-react";
+import { Input } from "./ui/Input";
+import { Card, CardContent } from "./ui/Card";
 
 interface UserListProps {
   users: User[];
@@ -12,31 +14,49 @@ function UserList() {
   return (
     <div className="p-6 space-y-4">
       <h1 className="text-2xl font-bold">Users</h1>
+      <Input
+        placeholder="search using name or email..."
+        className="border-1 focus:!ring-0 !w-80"
+      />
       <div className="grid gap-4 md:grid-cols-1 lg:grid-cols-2">
-        <div className="flex items-center justify-between p-4 border rounded-lg shadow-sm bg-white">
-          {/* Avatar */}
-          <div className="flex items-center space-x-4">
-            <span className="w-12 h-12 rounded-full bg-gray-200 flex items-center justify-center text-xl font-bold">
-              P
-            </span>
-            <div>
-              <p className="font-medium">{"Parsa"}</p>
-              <p className="text-sm text-gray-500">Papymankan</p>
-              <p className="text-sm text-gray-500">parsa85rostami@gmail.com</p>
-              <p className="text-sm text-gray-500">09188786856</p>
-            </div>
-          </div>
+        <Card className="border-gray-200 !shadow-md">
+          <CardContent>
+            <div className="flex items-center justify-between rounded-lg bg-white">
+              {/* Avatar */}
+              <div className="flex items-center space-x-4">
+                <span className="w-12 h-12 rounded-full bg-gray-200 flex items-center justify-center text-xl font-bold">
+                  P
+                </span>
+                <div>
+                  <p className="font-medium">{"Parsa"}</p>
+                  <p className="text-sm text-gray-500">Papymankan</p>
+                  <p className="text-sm text-gray-500">
+                    parsa85rostami@gmail.com
+                  </p>
+                  <p className="text-sm text-gray-500">09188786856</p>
+                </div>
+              </div>
 
-          {/* Actions */}
-          <div className="flex sm:flex-row flex-col sm:space-x-2 sm:space-y-0 space-y-1">
-            <Button variant="outline" size="sm" className=" cursor-pointer hover:bg-gray-100">
-              <PencilIcon className="w-4 h-4 mr-1" /> Edit
-            </Button>
-            <Button variant="default" size="sm" className="bg-red-600 text-white hover:bg-red-600/70 cursor-pointer">
-              <TrashIcon className="w-4 h-4 mr-1" /> Delete
-            </Button>
-          </div>
-        </div>
+              {/* Actions */}
+              <div className="flex sm:flex-row flex-col sm:space-x-2 sm:space-y-0 space-y-1">
+                <Button
+                  variant="outline"
+                  size="sm"
+                  className=" cursor-pointer hover:bg-gray-100"
+                >
+                  <PencilIcon className="w-4 h-4 mr-1" /> Edit
+                </Button>
+                <Button
+                  variant="default"
+                  size="sm"
+                  className="bg-red-600 text-white hover:bg-red-600/70 cursor-pointer"
+                >
+                  <TrashIcon className="w-4 h-4 mr-1" /> Delete
+                </Button>
+              </div>
+            </div>
+          </CardContent>
+        </Card>
       </div>
     </div>
   );
