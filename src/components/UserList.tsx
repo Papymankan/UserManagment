f5397d1f899ae5c30286706b47e19f940ce579d4
Papymankan <parsa84rostami@gmail.com>
2025-09-30 16:12:33 +0330

    feat(search) : add searching

--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -1,5 +1,7 @@
+"import"
+
 import { User } from "@/types";
-import React from "react";
+import React, { useState } from "react";
 import { Input } from "./ui/Input";
 import UserCard from "./UserCard";
 import Loader from "./Loader";
@@ -12,18 +14,27 @@ interface UserListProps {
 }
 
 function UserList({ users, loading, onEdit, onDelete }: UserListProps) {
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const filteredUsers = users.filter(
+    (u) =>
+      u.name.toLowerCase().includes(searchQuery.toLowerCase()) ||
+      u.email.toLowerCase().includes(searchQuery.toLowerCase())
+  );
   return (
     <div className="p-6 space-y-4">
       <h1 className="text-2xl font-bold">Users</h1>
       <Input
         placeholder="search using name or email..."
         className="border-1 focus:!ring-0 !w-80"
+        value={searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value)}
       />
       <div className="grid gap-4 md:grid-cols-1 lg:grid-cols-2">
         {loading ? (
           <Loader />
         ) : (
-          users.map((user) => (
+          filteredUsers.map((user) => (
             <UserCard
               key={user.id}
               user={user}
